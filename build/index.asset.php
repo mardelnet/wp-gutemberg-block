d4d4e24756283ea7a33b8857a7e88ea4387105d5
Pedro Figueroa <figueroash@gmail.com>
2024-03-13 13:37:22 +0100

    Create new basic block

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '79882251100c5ec553a1');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5b92a0f7306642841bd9');
