cd0dd3cefaedda06129fae373fec93126a16e538
Pedro Figueroa <figueroash@gmail.com>
2024-03-13 19:43:14 +0100

    Add carousel

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '395052855bc4a4e72834');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e6c9f07849442211a410');
--- src/edit.js
+++ src/edit.js
@@ -4,17 +4,31 @@ import { useState } from '@wordpress/element';
 import { onChangeTitle, onChangeContent, setImageAttributes, addPressNote, removePressNote } from './pressNoteFunctions';
 import PressNoteEditorItem from './PressNoteEditorItem';
 import './editor.scss';
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function Edit({ attributes, setAttributes }) {
     const [pressNotes, setPressNotes] = useState(attributes.pressNotes || []);
     const blockProps = useBlockProps();
+
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
 
     return (
         <div {...blockProps}>
             <BlockControls>
                 <button onClick={() => addPressNote(pressNotes, setPressNotes, setAttributes)}>Add Press Note</button>
             </BlockControls>
-            {pressNotes.map((pressNote, index) => (
+
+            <Slider {...settings}>
+              {pressNotes.map((pressNote, index) => (
                 <PressNoteEditorItem
                     key={index}
                     pressNote={pressNote}
@@ -24,7 +38,8 @@ export default function Edit({ attributes, setAttributes }) {
                     pressNotes={pressNotes}
                     setPressNotes={setPressNotes}
                 />
-            ))}
+              ))}
+            </Slider>
         </div>
     );
 }
