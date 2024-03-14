2f518dcd98a85bf7424413efcf29a36f62933351
Pedro Figueroa <figueroash@gmail.com>
2024-03-14 18:44:29 +0100

    Ignore and remove build files

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0076855b50b26b5848c6');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-components'), 'version' => '5291fd91c1d1da100299');
