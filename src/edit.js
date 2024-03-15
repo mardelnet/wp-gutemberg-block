efddab78bcaa0ef4735bf71e730ab895da852c1f
Pedro Figueroa <figueroash@gmail.com>
2024-03-15 14:40:34 +0100

    Add new press note on mount

--- src/edit.js
+++ src/edit.js
@@ -1,6 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, BlockControls } from '@wordpress/block-editor';
-import { useState } from '@wordpress/element';
+import { useState, useEffect } from '@wordpress/element';
 import { addPressNote, addFeaturedPressNote, removeFeaturedPressNote } from './utils/pressNoteFunctions';
 import { sliderSettingsWithArrows } from './utils/sliderSettings'
 
@@ -15,6 +15,12 @@ export default function Edit({ attributes, setAttributes }) {
     const [pressNotes, setPressNotes] = useState(attributes.pressNotes || []);
     const [featuredIndex, setFeaturedIndex] = useState(attributes.featuredPressNote);
     const blockProps = useBlockProps();
+
+    useEffect(() => {
+      if( pressNotes.length === 0 ) {
+        addPressNote(pressNotes, setPressNotes, setAttributes);
+      }
+    }, []);
 
     return (
         <div {...blockProps}>
